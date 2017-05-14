40ddc590bb36198dc124e91272e78576b753c393
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-05-14 17:47:55 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var Joi = require('joi');
-var console = require('tracer').colorConsole();
+var tracer = require('tracer').colorConsole();
 var express = require('express');
 
 
@@ -16,8 +16,7 @@ var Server = function (config) {
     };
 
     var init = function (config) {
-        mongoose.connect(config.mongo.connectionString);
-        
+        mongoose.connect(config.mongo.connectionString);        
         var Resource = mongoose.model(
           'Resource', new ResourceSchema(config.mongo.collection)
         );
@@ -37,39 +36,41 @@ var Server = function (config) {
         }
         
         app.get('/:number', (req, res) => {
-            var requestId = getRequestId();            
+            var requestId = getRequestId();
+            console.time(requestId);
+            
             var number = req.params.number;
-            console.log('number:', number, 'requestId:', requestId);
+            tracer.log(requestId, 'number:', number);
 
             finder.findCodeForNumber(number)
                 .then((doc) => {
-                    console.log(requestId, doc)
+                    tracer.log(requestId, doc)
                     if (doc) {
                         res.json(doc);
                     } else {
                         res.status(404).json({status: 'Not Found'});
                     }
-
+                    console.timeEnd(requestId);
                 })
                 .catch((err) => {
-                    console.err(requestId, err)
+                    tracer.err(requestId, err)
                     res.status(500).json({status: 'Error'});
                 });
         });
 
         app.listen(config.port, () => {
-            console.log('app listening on port ' + config.port);
+            tracer.log('app listening on port ' + config.port);
         });
     };
 
     this.start = function () {
         validate((err, config) => {
             if (err) {
-                console.log('config.js have errors', err);
+                tracer.log('config.js have errors', err);
                 return;
             }
             
-            console.log('config.js validated successfully!');
+            tracer.log('config.js validated successfully!');
             init(config);    
 
         });
