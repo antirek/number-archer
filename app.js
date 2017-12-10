28f1adb3715f399c76fcd5505a7ca4587a96199b
Sergey Dmitriev <serge.dmitriev@gmail.com>
2017-12-10 19:15:06 +0700

    fix

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ var FinderService = require('./controllers/FinderService');
 var service = FinderService(Resource, Finder);
 
 numberArcher.prepare(service);
-    
+
 numberArcher.app.listen(config.port, () => {
+    console.log('config:', config);
     console.log('Your server is listening on port %d (http://localhost:%d)', config.port, config.port);
     console.log('Swagger-ui is available on http://localhost:%d/docs', config.port);
 });
